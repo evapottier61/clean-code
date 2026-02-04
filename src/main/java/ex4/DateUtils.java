dd3c89084a3e6c4531847124af94f2ec4d8a32be
Eva POTTIER <eva.pottier@etu.unicaen.fr>
2026-02-04 14:26:26 +0100

    ex4

--- src/main/java/ex4/DateUtils.java
+++ src/main/java/ex4/DateUtils.java
@@ -4,16 +4,27 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class DateUtils {
-    
-    public static String format(String pattern, Date date) {
+    /**
+     * Default format for a date
+     */
+    private static final String DEFAULT_FORMAT = "dd/MM/yyyy HH:mm:ss";
 
+    /** Formatte la date en String avec un pattern
+     * @param date date
+     * @param pattern pattern
+     * @return String
+     */
+    public static String format(String pattern, Date date) {
         SimpleDateFormat formateur = new SimpleDateFormat(pattern);
         return formateur.format(date);
     }
 
+    /** Formate la date en String avec le pattern par défaut
+     * @param date date à formater
+     * @return String
+     */
     public static String formatDefaut(Date date) {
-
-        SimpleDateFormat formateur = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-        return formateur.format(date);
+        return format(DEFAULT_FORMAT, date);
     }
 }
+
