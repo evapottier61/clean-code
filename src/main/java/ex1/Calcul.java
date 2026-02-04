d8b3040f8feae9b5486905972d01a3c04896c85a
Eva POTTIER <eva.pottier@etu.unicaen.fr>
2026-02-04 10:49:48 +0100

    clean code and add javadoc

---
+++ src/main/java/ex1/Calcul.java
@@ -0,0 +1,22 @@
+package ex1;
+
+public class Calcul {
+
+    /**
+     * @param a integer to add
+     * @param b integer to add
+     * @return the sum of the two parameters a and b
+     */
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    /**
+     * @param a integer to substract
+     * @param b integer to substract
+     * @return the substraction of the two parameters a and b
+     */
+    public int substract(int a, int b) {
+        return a - b;
+    }
+}
---
+++ src/main/java/ex1/Entreprise.java
@@ -0,0 +1,39 @@
+package ex1;
+
+import java.util.Date;
+
+public class Entreprise {
+
+    /**
+     * The maximum capital which is 3000000
+     */
+    public static final int capitalMax = 3000000;
+
+    /**
+     * The company's siret
+     */
+    public int siret;
+
+    /**
+     * The company's name
+     */
+    public String nom;
+
+    /**
+     * The company's address
+     */
+    public String adresse;
+
+    /**
+     * The company's creation date
+     */
+    public Date dateCreation;
+
+    /**
+     * Print the company's status
+     */
+    public void afficherStatut() {
+
+    }
+
+}
